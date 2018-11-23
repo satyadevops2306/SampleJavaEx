9e42c15502b0a31bf593ff573edf38077822bbb9
root <root@ip-172-31-20-154.ap-south-1.compute.internal>
2018-11-23 18:57:06 +0000

    Added new files

---
+++ src/main/java/com/satya/App.java
@@ -0,0 +1,14 @@
+package com.satya;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
